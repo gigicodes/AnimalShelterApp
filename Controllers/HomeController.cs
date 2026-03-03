bb1e797742c663e9a3b6c191042dd50846d779f2
gigicodes <gifftchiedozie@gmail.com>
2026-03-03 16:53:10 +0100

    Created a contact us page, added the "animal of the day" feature.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,19 +1,35 @@
 using System.Diagnostics;
 using AnimalShelterApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using AnimalShelterApp.Services;
 
 namespace AnimalShelterApp.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AnimalService _animalService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AnimalService animalService)
         {
             _logger = logger;
+            _animalService = animalService;
         }
 
         public IActionResult Index()
+        {
+            var animals = _animalService.GetAllAnimals()
+                                        .Where(a => !a.IsAdopted)
+                                        .ToList();
+
+            var random = new Random();
+            var animalOfTheDay = animals.Count > 0
+                ? animals[random.Next(animals.Count)]
+                : null;
+
+            return View(animalOfTheDay);
+        }
+        public IActionResult ContactUs()
         {
             return View();
         }
